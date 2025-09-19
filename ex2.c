24a9d2b02beb5b5451d27ec5f9e2fbca9c7c833e
guerreirof25-crypto <guerreirof25@gmail.com>
2025-09-19 16:02:07 -0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -14,9 +14,8 @@ struct personagem preencheA (void){
     scanf("%d",&p.experiencia);
     return p;
 }
-int main() {
+void main(void) {
     struct personagem p1 = preencheA();
     struct personagem p2 = preencheA();
     printf("(%d,%d,%d) vs (%d,%d,%d)\n",p1.forca,p1.energia,p1.experiencia,p2.forca,p2.energia,p2.experiencia);
-   return 0;
 }
