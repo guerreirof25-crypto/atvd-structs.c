ab8632ce872f62b0b7d712db5f2694f23df8cfca
guerreirof25-crypto <guerreirof25@gmail.com>
2025-09-22 12:36:06 -0300

    Update ex9.c

--- ex9.c
+++ ex9.c
@@ -1,29 +1,51 @@
-#include <stdio.h>
+#include<stdio.h>
 
-struct ponto {
+struct ponto{
     int x;
     int y;
 };
 
-struct personagem {
+struct personagem{
     int id;
     struct ponto pos;
     int pontuacao;
 };
 
-int main(void) {
-    struct personagem p[10];
 
-    for (int i = 0; i < 10; i++) {
-        printf("digite a id: ");
-        scanf("%d", &p[i].id);
+struct personagem preencheb(void){
+    struct personagem p;
+    printf("digite a id: ");
+    scanf("%d",&p.id);
+    printf("digite a pontuação: ");
+    scanf("%d",&p.pontuacao);
+    printf("digite a coordenada x: ");
+    scanf("%d",&p.pos.x);
+    printf("digite a coordenada y: ");
+    scanf("%d",&p.pos.y);
+    return p;
+}
 
-        printf("digite a posição (x y): ");
-        scanf("%d %d", &p[i].pos.x, &p[i].pos.y);
-
-        printf("digite a pontuação: ");
-        scanf("%d", &p[i].pontuacao);
+    
+void vetor(struct personagem p[],int n){
+    for(int i=0;i<n;i++){
+        p[i]=preencheb();
     }
+}
 
+void exibe(struct personagem p[], int n ){
+    for(int i=0;i<n;i++){
+        printf("\n======================\n");
+        printf("personagem: %d\n", i+1);
+        printf("id: %d\n", p[i].id);
+        printf("posição: (%d,%d)\n",p[i].pos.x,p[i].pos.y);
+        printf("pontuação: %d\n", p[i].pontuacao);
+        printf("======================");
+    }
+}
+
+int main(){
+    struct personagem p[10];
+    vetor(p,10);
+    exibe(p,10);
     return 0;
 }
